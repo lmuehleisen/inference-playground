d54176d7854e567f342f4cd22f45bba31f88a7ef
Mishig Davaadorj <dmishig@gmail.com>
2024-07-29 13:30:23 +0200

    `Warm -> warm`

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,7 +4,7 @@ import type { PageServerLoad } from "./$types";
 import { env } from "$env/dynamic/private";
 
 export const load: PageServerLoad = async ({ fetch }) => {
-	const apiUrl = "https://huggingface.co/api/models?pipeline_tag=text-generation&inference=Warm&filter=conversational";
+	const apiUrl = "https://huggingface.co/api/models?pipeline_tag=text-generation&inference=warm&filter=conversational";
 	const HF_TOKEN = env.HF_TOKEN;
 
 	const res = await fetch(apiUrl, {
