f08464b30b2cd8d31da6c6fd14cb3ebfd2c46cb8
Mishig Davaadorj <dmishig@gmail.com>
2024-09-12 13:24:31 +0200

    accept `/?__theme=dark`

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import "../app.css";
+	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
+
+	$: if (browser && $page.url.searchParams.get('__theme') === 'dark') {
+		document.documentElement.classList.add('dark');
+	}
 </script>
 
 <slot></slot>
