47215b195c632929d920bde97eed0bbff2a768a8
Victor Mustar <victor.mustar@gmail.com>
2024-09-23 13:24:39 +0200

    icon

--- src/lib/components/InferencePlayground/InferencePlayground.svelte
+++ src/lib/components/InferencePlayground/InferencePlayground.svelte
@@ -323,10 +323,10 @@
 						target="_blank"
 						class="flex items-center gap-0.5 text-gray-500 hover:text-gray-700 dark:hover:text-gray-400"
 					>
-						Model page
 						<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"
 							><path fill="currentColor" d="M10 6v2h12.59L6 24.59L7.41 26L24 9.41V22h2V6H10z" /></svg
 						>
+						Model page
 					</a>
 				</div>
 			</div>
