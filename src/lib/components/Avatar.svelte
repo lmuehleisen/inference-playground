9c0f990605f07affd5f6417df371f76fcbf8b66f
Victor Mustar <victor.mustar@gmail.com>
2024-10-10 15:18:32 +0200

    fix: Improve formatting and indentation in Avatar.svelte

--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
-    export let orgName: string;
-    export let size: number = 12;
+	export let orgName: string;
+	export let size: number = 12;
 
-    async function getAvatarUrl(orgName: string) {
-        const url = `https://huggingface.co/api/organizations/${orgName}/avatar`;
-        const res = await fetch(url);
-        if (!res.ok) {
-            console.error(`Error getting avatar url for org: ${orgName}`, res.status, res.statusText);
-            return;
-        }
-        const json = await res.json();
-        const { avatarUrl } = json;
-        return avatarUrl;
-    }
+	async function getAvatarUrl(orgName: string) {
+		const url = `https://huggingface.co/api/organizations/${orgName}/avatar`;
+		const res = await fetch(url);
+		if (!res.ok) {
+			console.error(`Error getting avatar url for org: ${orgName}`, res.status, res.statusText);
+			return;
+		}
+		const json = await res.json();
+		const { avatarUrl } = json;
+		return avatarUrl;
+	}
 </script>
 
 {#await getAvatarUrl(orgName)}
-    <div class="size-{size} flex-none rounded bg-gray-200"></div>
+	<div class="size-{size} flex-none rounded bg-gray-200"></div>
 {:then avatarUrl}
-    <img class="size-{size} flex-none rounded bg-gray-200 object-cover" src={avatarUrl} alt="{orgName} avatar" />
+	<img class="size-{size} flex-none rounded bg-gray-200 object-cover" src={avatarUrl} alt="{orgName} avatar" />
 {:catch}
-    <div class="size-{size} flex-none rounded bg-gray-200"></div>
+	<div class="size-{size} flex-none rounded bg-gray-200"></div>
 {/await}
