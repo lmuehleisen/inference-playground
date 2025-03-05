209fd1ce598a38805364bfaf5de10044e8044565
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-03-05 16:27:51 +0000

    update tailwind

--- src/lib/components/InferencePlayground/InferencePlaygroundHFTokenModal.svelte
+++ src/lib/components/InferencePlayground/InferencePlaygroundHFTokenModal.svelte
@@ -108,6 +108,8 @@
 					href="https://huggingface.co/settings/tokens/new?globalPermissions=inference.serverless.write&tokenType=fineGrained"
 					tabindex="-1"
 					target="_blank"
+					<<<<<<<
+					HEAD
 					class="rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 focus:outline-hidden dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
 					>Create new token</a
 				>
