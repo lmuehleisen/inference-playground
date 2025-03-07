52803ee0964547bdba34791c65067838834ba4b4
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-03-07 10:32:00 +0000

    simpler stores

--- src/lib/stores/models.ts
+++ src/lib/stores/models.ts
@@ -1,33 +1,8 @@
 import { page } from "$app/stores";
 import type { ModelEntryWithTokenizer } from "$lib/components/InferencePlayground/types";
-import { effect } from "$lib/utils/effect";
-import { get, writable } from "svelte/store";
+import { readable } from "svelte/store";
 
-function createModelsStore() {
-	let hasStarted = false;
-	const store = writable<ModelEntryWithTokenizer[]>([]);
-
-	function init() {
-		store.set(get(page)?.data.models ?? []);
-		hasStarted = true;
-	}
-
-	const subscribe: (typeof store)["subscribe"] = (...args) => {
-		if (!hasStarted) init();
-		hasStarted = true;
-		const unsubs = [
-			effect(page, $page => {
-				store.set($page.data.models);
-			}),
-			store.subscribe(...args),
-		];
-
-		return () => {
-			unsubs.forEach(unsub => unsub());
-		};
-	};
-
-	return { ...store, subscribe };
-}
-
-export const models = createModelsStore();
+export const models = readable<ModelEntryWithTokenizer[]>(undefined, set => {
+	const unsub = page.subscribe($p => set($p.data.models));
+	return unsub;
+});
--- src/lib/stores/session.ts
+++ src/lib/stores/session.ts
@@ -6,66 +6,50 @@ import {
 import type { Conversation, ConversationMessage, Session } from "$lib/components/InferencePlayground/types";
 
 import { models } from "$lib/stores/models";
-import { partialSet, safePage } from "$lib/utils/store";
+import { safePage } from "$lib/utils/store";
 import { get, writable } from "svelte/store";
 
-export function createSessionStore() {
-	let hasStarted = false;
-	const store = writable<Session>();
-
-	// Init is needed, otherwise there are stale values coming from page.
-	function init() {
-		const startMessageUser: ConversationMessage = { role: "user", content: "" };
-		const modelIdsFromQueryParam = get(safePage)?.url?.searchParams?.get("modelId")?.split(",");
-		const modelsFromQueryParam = modelIdsFromQueryParam?.map(id => get(models).find(model => model.id === id));
-		const systemMessage: ConversationMessage = {
-			role: "system",
-			content: modelIdsFromQueryParam?.[0] ? (defaultSystemMessage?.[modelIdsFromQueryParam[0]] ?? "") : "",
-		};
-
-		store.set({
-			conversations: [
-				{
-					model: get(models).find(m => FEATURED_MODELS_IDS.includes(m.id)) ??
-						get(models)[0] ?? {
-							id: "",
-							downloads: 0,
-							gated: false,
-							likes: 0,
-							name: "",
-							private: false,
-							tokenizerConfig: {},
-							updatedAt: new Date(),
-						},
-					config: { ...defaultGenerationConfig },
-					messages: [{ ...startMessageUser }],
-					systemMessage,
-					streaming: true,
-				},
-			],
-		});
-
-		if (modelsFromQueryParam?.length) {
-			const conversations = modelsFromQueryParam.map(model => {
-				return {
-					model,
-					config: { ...defaultGenerationConfig },
-					messages: [{ ...startMessageUser }],
-					systemMessage,
-					streaming: true,
-				};
-			}) as [Conversation] | [Conversation, Conversation];
-			partialSet(store, { conversations });
-		}
-	}
-
-	const subscribe: (typeof store)["subscribe"] = (...args) => {
-		if (!hasStarted) init();
-		hasStarted = true;
-		return store.subscribe(...args);
+export const session = writable<Session>(undefined, (set, update) => {
+	const startMessageUser: ConversationMessage = { role: "user", content: "" };
+	const modelIdsFromQueryParam = get(safePage)?.url?.searchParams?.get("modelId")?.split(",");
+	const modelsFromQueryParam = modelIdsFromQueryParam?.map(id => get(models).find(model => model.id === id));
+	const systemMessage: ConversationMessage = {
+		role: "system",
+		content: modelIdsFromQueryParam?.[0] ? (defaultSystemMessage?.[modelIdsFromQueryParam[0]] ?? "") : "",
 	};
 
-	return { ...store, subscribe };
-}
+	set({
+		conversations: [
+			{
+				model: get(models).find(m => FEATURED_MODELS_IDS.includes(m.id)) ??
+					get(models)[0] ?? {
+						id: "",
+						downloads: 0,
+						gated: false,
+						likes: 0,
+						name: "",
+						private: false,
+						tokenizerConfig: {},
+						updatedAt: new Date(),
+					},
+				config: { ...defaultGenerationConfig },
+				messages: [{ ...startMessageUser }],
+				systemMessage,
+				streaming: true,
+			},
+		],
+	});
 
-export const session = createSessionStore();
+	if (modelsFromQueryParam?.length) {
+		const conversations = modelsFromQueryParam.map(model => {
+			return {
+				model,
+				config: { ...defaultGenerationConfig },
+				messages: [{ ...startMessageUser }],
+				systemMessage,
+				streaming: true,
+			};
+		}) as [Conversation] | [Conversation, Conversation];
+		update(s => ({ ...s, conversations }));
+	}
+});
