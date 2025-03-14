7260a8063a39b83fcf30aba7e70f7bd4a6cb07e4
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-03-14 12:47:13 +0000

    fix models 401

--- src/routes/api/models/+server.ts
+++ src/routes/api/models/+server.ts
@@ -18,9 +18,22 @@ export const GET: RequestHandler = async ({ fetch }) => {
 	const HF_TOKEN = env.HF_TOKEN;
 
 	const res = await fetch(apiUrl, {
+		credentials: "include",
 		headers: {
-			Authorization: `Bearer ${HF_TOKEN}`,
+			"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0",
+			"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+			"Accept-Language": "en-US,en;q=0.5",
+			"Upgrade-Insecure-Requests": "1",
+			"Sec-Fetch-Dest": "document",
+			"Sec-Fetch-Mode": "navigate",
+			"Sec-Fetch-Site": "none",
+			"Sec-Fetch-User": "?1",
+			"Priority": "u=0, i",
+			"Pragma": "no-cache",
+			"Cache-Control": "no-cache",
 		},
+		method: "GET",
+		mode: "cors",
 	});
 
 	if (!res.ok) {
