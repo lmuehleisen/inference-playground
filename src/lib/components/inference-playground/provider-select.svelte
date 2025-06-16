357ab93f19c04d917611e0f064ccb9e8c35e67f9
Thomas G. Lopes <26071571+TGlide@users.noreply.github.com>
2025-06-16 22:08:28 +0100

    fix

--- src/lib/components/inference-playground/provider-select.svelte
+++ src/lib/components/inference-playground/provider-select.svelte
@@ -18,7 +18,7 @@
 
 	function reset(providers: typeof conversation.model.inferenceProviderMapping) {
 		const validProvider = providers.find(p => p.provider === conversation.data.provider);
-		if (validProvider || conversation.data.provider === "automatic") return;
+		if (validProvider || conversation.data.provider === "auto") return;
 		if (providers) {
 			conversation.update({ provider: randomPick(providers)?.provider });
 		} else {
@@ -99,19 +99,19 @@
 	</button>
 
 	<div {...select.content} class="rounded-lg border bg-gray-100 dark:border-gray-700 dark:bg-gray-800">
-		{#snippet option(provider: string)}
+		{#snippet option(provider: string, label?: string)}
 			<div {...select.getOption(provider)} class="group block w-full p-1 text-sm dark:text-white">
 				<div
 					class="flex items-center gap-2 rounded-md px-2 py-1.5 group-data-[highlighted]:bg-gray-200 dark:group-data-[highlighted]:bg-gray-700"
 				>
 					<IconProvider {provider} />
-					{formatName(provider)}
+					{formatName(label ?? provider)}
 				</div>
 			</div>
 		{/snippet}
 		{#each conversation.model.inferenceProviderMapping as { provider, providerId } (provider + providerId)}
 			{@render option(provider)}
 		{/each}
-		{@render option("automatic")}
+		{@render option("auto", "Choose automatically")}
 	</div>
 </div>
